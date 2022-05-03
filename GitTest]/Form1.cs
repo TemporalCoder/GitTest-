14ffeb276d7588ddb251dfe9ff7b5d5734d8dca2
Doug Parr <temporalcoder@gmail.com>
2022-05-03 13:55:54 +0100

    update button

--- GitTest]/Form1.cs
+++ GitTest]/Form1.cs
@@ -16,5 +16,11 @@ namespace GitTest_
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //some code
+
+        }
     }
 }
